f9c1e3502b6bf4802be972974191c70fb0a8be12
mustafafinnentrop-stack <266660022+mustafafinnentrop-stack@users.noreply.github.com>
2026-05-04 00:55:19 -0400

    feat: add capacitor ios support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.gerki.app',
+  appName: 'Gerki',
+  webDir: 'out/renderer'
+};
+
+export default config;
